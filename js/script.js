2cd32a7f3cf7ea9d5771badf9e3a6e5d607c6a0a
dxoof <soplifan.rifamas@gmail.com>
2021-09-02 15:37:35 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let anp = document.querySelector('.inp');
+
+let jquery = document.querySelector('.jquery');
+let popper = document.querySelector('.popper');
+let bootstrap = document.querySelector('.bootstrap');
+
+console.log(jquery + popper + bootstrap);
+
