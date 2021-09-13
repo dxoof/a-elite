436aace24253d2bec688b980d7a78b9b4b90a1e0
dxoof <soplifan.rifamas@gmail.com>
2021-09-13 17:51:06 +0700

    ml wtf

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let anp = document.querySelector('.inp');
-
-let jquery = document.querySelector('.jquery');
-let popper = document.querySelector('.popper');
-let bootstrap = document.querySelector('.bootstrap');
-
-console.log(jquery + popper + bootstrap);
-
